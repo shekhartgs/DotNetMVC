d2d6dee5e303e53e16a78070badb0e40ae180766
Sekhar <shekhartgs@gmail.com>
2021-09-01 00:10:11 +0530

    MVC Demo

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBusiness;
+
+namespace MVC.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        public ActionResult Index()
+        {
+            EmployeeBusiness employeeBusiness = new EmployeeBusiness();
+            List<Employee> employees = employeeBusiness.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
